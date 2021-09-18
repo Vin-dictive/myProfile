6837dbf7357eebf8f17221352af62737a89c1da0
Vinay Valson <vinay.valson@gmail.com>
2021-09-18 08:58:14 +0530

    Added Three.js Components for testing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
 import TechStack from "./Components/TechStack.js"
+import ThreeTest from "./Components/ThreeTest";
 
 class App extends Component {
   constructor(props) {
@@ -48,6 +49,7 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <TechStack data={this.state.resumeData.portfolio} />
+        <ThreeTest data={this.state.resumeData.portfolio}/>
         <Portfolio data={this.state.resumeData.portfolio} />
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
---
+++ src/Components/ThreeTest.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three'
+import React, { Component,useRef, useMemo, useLayoutEffect } from 'react'
+import { Canvas } from '@react-three/fiber'
+import niceColors from 'nice-color-palettes'
+import { OrbitControls } from '@react-three/drei'
+
+class ThreeTest extends Component {
+    
+  render() {
+    if (!this.props.data) return null;
+
+    const size = 125000
+    const tempObject = new THREE.Object3D()
+    const tempColor = new THREE.Color()
+    const colors = new Array(size).fill().map(() => niceColors[17][Math.floor(Math.random() * 5)])
+    
+    function Boxes() {
+        const colorArray = useMemo(() => Float32Array.from(new Array(size).fill().flatMap((_, i) => tempColor.set(colors[i]).toArray())), [])
+        const ref = useRef()
+        useLayoutEffect(() => {
+          let i = 0
+          for (let x = 0; x < 50; x++)
+            for (let y = 0; y < 50; y++)
+              for (let z = 0; z < 50; z++) {
+                const id = i++
+                tempObject.position.set(25 - x, 25 - y, 25 - z)
+                tempObject.updateMatrix()
+                ref.current.setMatrixAt(id, tempObject.matrix)
+              }
+          ref.current.instanceMatrix.needsUpdate = true
+        }, [])
+      
+        return (
+          <instancedMesh ref={ref} args={[null, null, size]}>
+            <boxBufferGeometry attach="geometry" args={[0.15, 0.15, 0.15]}>
+              <instancedBufferAttribute attachObject={['attributes', 'color']} args={[colorArray, 3]} />
+            </boxBufferGeometry>
+            <meshLambertMaterial attach="material" vertexColors={THREE.VertexColors} />
+          </instancedMesh>
+        )
+    }
+
+    return (
+      <section id="threeTest" className="threeTest-panel">
+        <Canvas linear camera={{ position: [0, 0, 0.1] }}>
+            <ambientLight />
+            <directionalLight position={[150, 150, 150]} intensity={0.55} />
+            <Boxes />
+            <OrbitControls enablePan={false} autoRotate />
+        </Canvas>
+      </section>
+    );
+  }
+}
+
+export default ThreeTest;
