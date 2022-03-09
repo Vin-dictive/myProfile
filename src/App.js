e074361fbd9c726937fe9350c2e3f8bf791153fb
Vinay Valson <34976549+Vin-dictive@users.noreply.github.com>
2022-03-09 16:21:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 import TechStack from './Components/TechStack';
-// import ThreeTest from './Components/ThreeTest';
+import ThreeTest from './Components/ThreeTest';
 import HexGridBackground from './Components/HexGridBackground';
 
 class App extends Component {
@@ -67,7 +67,7 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <TechStack data={this.state.resumeData.portfolio} />
-//         <ThreeTest data={this.state.resumeData.portfolio} />
+        <ThreeTest data={this.state.resumeData.portfolio} />
         <Portfolio data={this.state.resumeData.portfolio} />
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
